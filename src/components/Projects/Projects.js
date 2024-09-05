c7ccd35591548c54bf085141e051cd48a0136380
badguyOP <75782230+badguyOP@users.noreply.github.com>
2024-09-05 23:15:28 +0530

    Final Production

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-import foundry from "../../Assets/Projects/foundry.png";
+import foundry from "../../Assets/Projects/foundry.jpeg";
 
 function Projects() {
   return (
